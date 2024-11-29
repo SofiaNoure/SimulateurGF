dda58ff8cc11d37745c97cf578a27e18c88c1165
Sofia N <sofia@MacBook-Pro-de-Sofia.local>
2024-11-29 22:49:28 +0100

    first

--- Simulateur_GF.c
+++ Simulateur_GF.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
     printf("Hello everyone, Welcome to GitHub!\n");
     printf("ana hna rani nsayi berk LOL !");
     printf("a");
-
+    
     printf("c");
+   
   
     return 0;
 }
