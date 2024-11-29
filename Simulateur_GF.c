47691a848618bf01c6178dff6ce3701017550d9a
Sofia N <sofia@MacBook-Pro-de-Sofia.local>
2024-11-29 11:51:21 +0100

    first commit

---
+++ Simulateur_GF.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  // Inclusion de la bibliothèque standard pour les entrées/sorties
+
+int main() {
+    // Affiche le message à l'écran
+    printf("Hello, World!\n");
+
+    return 0;  // Indique que le programme s'est terminé avec succès
+}
