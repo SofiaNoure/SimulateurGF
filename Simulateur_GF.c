2896221841c7412a61c3b5c4a9bde1cac94d46a6
sofia <sofianoure@gmail.com>
2024-11-29 22:28:50 +0100

    Update Simulateur_GF.c

--- Simulateur_GF.c
+++ Simulateur_GF.c
@@ -4,12 +4,8 @@ int main() {
 
     printf("Hello everyone, Welcome to GitHub!\n");
     printf("ana hna rani nsayi berk LOL !");
-<<<<<<< HEAD
+
     printf("c");
-    printf("a");
-=======
-    
->>>>>>> 7ad3535133c4ed60c55a7bf58e30d42dc0c9246b
-    
+  
     return 0;
 }
