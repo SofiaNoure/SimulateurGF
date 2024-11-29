c99afd97f22d02a86be5f7fc029b5b765a9a1d41
SofiaNoure <sofianoure@gmail.com>
2024-11-29 11:56:52 +0100

    Update Simulateur_GF.c

--- Simulateur_GF.c
+++ Simulateur_GF.c
@@ -1,8 +1,8 @@
-#include <stdio.h>  // Inclusion de la bibliothèque standard pour les entrées/sorties
+#include <stdio.h>  
 
 int main() {
-    // Affiche le message à l'écran
+    
     printf("Hello everyone, Welcome!\n");
 
-    return 0;  // Indique que le programme s'est terminé avec succès
+    return 0;  
 }
