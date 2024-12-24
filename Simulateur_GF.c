6d91abd7954d33d52faef94af82696b993a516e9
linadelh <lina35979@gmail.com>
2024-12-24 02:59:33 +0100

    Save local changes to Simulateur_GF.c

--- Simulateur_GF.c
+++ Simulateur_GF.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
+#define NBR_BLOCS 100 // Nombre total de blocs dans la mémoire
+#define FB 10 // Taille d'un bloc
 
-    printf("Hello everyone, Welcome to GitHub!\n");
-    printf("ana hna rani nsayi berk LOL !");
-    printf("a");
-    printf("b");
-    printf("c");
-    printf("d");
-    printf("e");
-    printf("f");
-    printf("g");
-    printf("H");
-  
-    return 0;
-}
+
+ typedef struct {
+  int Table_dallocation[NBR_BLOCS]; //libre 0 ou non 1
+  Produit Produits[FB];
+  struct BLOC* suivant; // Pointeur vers le bloc suivant
+ }BLOC ;
+
+ typedef struct {
+  char nom [21];
+  int id ;
+  float prix ;
+ }Produit ;
+
+ typedef struct {
+ char nom [21];
+ int taille ; //taille du fichier en bloc
+ int nbr_produit ;
+ int organisation_globale; //1 pour contigu et 0 pour chainee
+ BLOC* premier_bloc; // Adresse du premier bloc (mode chainee)
+ int organisation_interne ;// 1 pour triee et 0 pour non triee
+ }Meta ;
