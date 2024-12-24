0a55da53afa7338ff43515496984d57fda0b9178
linadelh <lina35979@gmail.com>
2024-12-24 10:31:14 +0100

    neww

--- Simulateur_GF.c
+++ Simulateur_GF.c
@@ -5,24 +5,25 @@
 #define NBR_BLOCS 100 // Nombre total de blocs dans la mémoire
 #define FB 10 // Taille d'un bloc
 
-typedef struct {
+ typedef struct {
   char nom [21];
   int id ;
   float prix ;
  }Produit ;
 
  typedef struct {
-  int Table_dallocation[NBR_BLOCS]; //libre 0 ou non 1
   Produit Produits[FB];
   struct BLOC* suivant; // Pointeur vers le bloc suivant
  }BLOC ;
+
 
  typedef struct {
  char nom [21];
  int taille ; //taille du fichier en bloc
  int nbr_produit ;
  int organisation_globale; //1 pour contigu et 0 pour chainee
- BLOC* premier_bloc; // Adresse du premier bloc (mode chainee)
  int organisation_interne ;// 1 pour triee et 0 pour non triee
+ BLOC* premier_bloc; // Adresse du premier bloc (mode chainee)
+ int Table_dallocation[NBR_BLOCS]; //libre 0 ou non 1
  }Meta ;
 
